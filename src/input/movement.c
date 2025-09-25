7ecf121ba59a9a8af72d980d54324e102174ae7f
Milos Mitkovic <mitkovic_m@hotmail.com>
2025-09-25 17:42:44 +0200

    Add small header fix

--- src/input/movement.c
+++ src/input/movement.c
@@ -6,52 +6,12 @@
 /*   By: mmitkovi <mmitkovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 13:21:20 by mmitkovi          #+#    #+#             */
-/*   Updated: 2025/09/25 17:29:10 by mmitkovi         ###   ########.fr       */
+/*   Updated: 2025/09/25 17:41:38 by mmitkovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //  WASD, rotate, collision
 
-#include "../../includes/cub3d.h"
-
-int	handle_close_win(int keycode, t_data *data)
-{
-	if (keycode == KEY_ESC)
-	{
-		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-		data->win_ptr = NULL;
-	}
-	return (0);
-}
-int	handle_keyrelease(int keycode, void *param)
-{
-	t_data	*data;
-	
-	data = (t_data *)param;
-	
-}
-
-int handle_keypress(int keycode, void *param)
-{
-	t_data	*data;
-
-	data = (t_data *)param;
-	if (keycode == KEY_ESC)
-		clean_exit(data);
-	if (keycode == KEY_W)
-		printf("Keypress: %d\n", keycode);
-	if (keycode == KEY_A)
-		printf("Keypress: %d\n", keycode);
-	if (keycode == KEY_S)
-		printf("Keypress: %d\n", keycode);
-	if (keycode == KEY_D)
-		printf("Keypress: %d\n", keycode);
-	if (keycode == R_ARW)
-		printf("Keypress: %d\n", keycode);
-	if (keycode == L_ARW)
-		printf("Keypress: %d\n", keycode);
-	return (0);
-}
 
 
 #include "../../includes/cub3d.h"
