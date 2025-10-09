0c37b9227588dff0a7798a87d34eb55ab383dc75
Milos Mitkovic <mitkovic_m@hotmail.com>
2025-10-09 12:39:57 +0200

    Add movement fix

--- src/input/movement.c
+++ src/input/movement.c
@@ -6,7 +6,7 @@
 /*   By: mmitkovi <mmitkovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 13:21:20 by mmitkovi          #+#    #+#             */
-/*   Updated: 2025/10/08 13:08:38 by mmitkovi         ###   ########.fr       */
+/*   Updated: 2025/10/09 12:37:26 by mmitkovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 
 void	move_forward(t_data *data)
 {
-	// key W
-	double nx = data->pos_x + data->player.walk_direction * data->player.walk_speed;
-	double ny = data->pos_y + data->player.walk_direction * data->player.walk_speed;
+	double	nx;
+	double	ny;
+
+	nx = data->pos_x + data->player.walk_direction * data->player.walk_speed;
+	ny = data->pos_y + data->player.walk_direction * data->player.walk_speed;
 	if (data->parser->map[(int)(data->pos_y)][(int)nx] == '0')
 		data->pos_x = nx;
 	if (data->parser->map[(int)(ny)][(int)data->pos_x] == '0')
@@ -26,34 +28,44 @@ void	move_forward(t_data *data)
 
 void	move_back(t_data *data)
 {
-	// key S
-	if ((data->parser->map[(int)(data->pos_y - data->player.walk_direction
-				* data->player.walk_speed)][(int)data->pos_x]) == '0')
-			data->pos_y -= data->pos_y * data->player.walk_speed;
-	// update x
-	if (data->parser->map[(int)(data->pos_y)][(int)(data->pos_x
-				- data->player.walk_direction * data->player.walk_speed)] == '0')
-			data->pos_x -= data->pos_x * data->player.walk_speed;
+	double	nx;
+	double	ny;
+
+	ny = data->pos_y - data->player.walk_direction * data->player.walk_speed;
+	nx = data->pos_x - data->player.walk_direction * data->player.walk_speed;
+	if (data->parser->map[(int)data->pos_y][(int)nx] == '0')
+		data->pos_x = nx;
+	if ((data->parser->map[(int)ny][(int)data->pos_x]) == '0')
+		data->pos_y = ny;
 }
 
 void	move_right(t_data *data)
 {
-	// key D
-	double nx = data->pos_x + data->player.walk_direction * data->player.walk_speed;
-	double ny = data->pos_y + data->player.walk_direction * data->player.walk_speed;
-	if (data->parser->map[(int)(data->pos_y)][(int)((int)(nx))])
+	double	nx;
+	double	ny;
+
+	nx = data->pos_x + data->player.walk_direction * data->player.walk_speed;
+	ny = data->pos_y + data->player.walk_direction * data->player.walk_speed;
+	if (data->parser->map[(int)data->pos_y][(int)nx] == '0')
 		data->pos_x = nx;
-	//check y
-	if (data->parser->map[(int)(ny)][(int)(data->pos_x)] == '0')
+	if (data->parser->map[(int)ny][(int)data->pos_x] == '0')
 		data->pos_y = ny;
-	
 }
 
 void	move_left(t_data *data)
 {
+	double	nx;
+	double	ny;
+
+	nx = data->pos_x - data->player.walk_direction * data->player.walk_speed;
+	ny = data->pos_y + data->player.walk_direction * data->player.walk_speed;
+	if (data->parser->map[(int)data->pos_y][(int)nx] == '0')
+		data->pos_x = nx;
+	if (data->parser->map[(int)ny][(int)data->pos_x] == '0')
+		data->pos_y = ny;
 }
 
-void move_player(t_data *data)
+void	move_player(t_data *data)
 {
 	// if (/*moving forward*/)
 	move_forward(data);
